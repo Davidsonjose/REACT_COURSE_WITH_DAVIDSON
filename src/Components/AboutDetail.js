3a385e757a812f9e674ade51ad3e53d0db5fdd0c
Davidson jose <91620722+Davidsonjose@users.noreply.github.com>
2022-02-26 01:53:31 +0100

    day 3 class ongoing

---
+++ src/Components/AboutDetail.js
@@ -0,0 +1,21 @@
+export const about = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZSUyMGltYWdlc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    description:
+      "I am a Figma designer and a Backend developer",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZmlsZSUyMGltYWdlc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    description:
+      "I am a front end developer.... ",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1601054704854-1a2e79dda4d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHByb2ZpbGUlMjBpbWFnZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    description:
+      "I am a UI/UX designer and a Backend developer",
+  },
+];
+
